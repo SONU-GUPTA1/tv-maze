1599445279d0339db7d2db7049839c3dd4b93e7d
SONU-GUPTA1 <sonukumarguptasonu546@gmail.com>
2022-09-28 23:30:21 +0530

    added files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import NavBar from "./Components/NavBar";
+import Search from "./Components/Search";
+import "./App.css";
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Search />
+    </>
+  );
+}
+
+export default App;
